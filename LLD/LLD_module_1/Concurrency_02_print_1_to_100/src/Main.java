6d1d69f456c24a33b11da9ba10a1a3e1ec3f2789
shikha-999 <yadav.shikha.ys@gmail.com>
2024-01-19 23:41:37 +0530

    concurrency 02 - print 1 to 100 in diff threads

--- LLD/LLD_module_1/Concurrency_02_print_1_to_100/src/Main.java
+++ LLD/LLD_module_1/Concurrency_02_print_1_to_100/src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        for(int i = 1; i <= 100; i++){
+            NumberPrinter numberPrinter = new NumberPrinter(i);
+            Thread thread = new Thread(numberPrinter);
+            thread.start();
+        }
     }
 }
