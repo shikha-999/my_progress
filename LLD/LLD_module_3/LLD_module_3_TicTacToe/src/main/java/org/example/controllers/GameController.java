b7623bcf0f65d378377652abe7d5467f6ee3629d
shikha-999 <yadav.shikha.ys@gmail.com>
2024-02-03 14:15:18 +0530

    TicTacToe1

---
+++ LLD/LLD_module_3/LLD_module_3_TicTacToe/src/main/java/org/example/controllers/GameController.java
@@ -0,0 +1,32 @@
+package org.example.controllers;
+
+import java.util.List;
+
+import org.example.models.Player;
+import org.example.models.Game;
+import org.example.strategies.winningStrategies.WinningStrategy;
+
+public class GameController {
+    //start the game
+    //difficulty required only for bot not all players
+    //hence not passing difficulty
+    public Game startGame(int boardDimension, List<Player> players,
+                          List<WinningStrategy> winningStrategies){
+        return null;
+    }
+    //play game
+    public void makeMove(Game game){
+        game.makeMove();
+    }
+    //display board
+    public void displayBoard(Game game){
+        game.printBoard();
+    }
+    //check winner
+    public Player checkWinner(Game game){
+        return game.getWinner();
+    }
+    public void undo(Game game){
+        game.undo();
+    }
+}
---
+++ LLD/LLD_module_3/LLD_module_3_TicTacToe/src/main/java/org/example/models/Move.java
@@ -0,0 +1,26 @@
+package org.example.models;
+
+//which player is making the move and where
+public class Move {
+    private Cell cell;
+    private Player player;
+
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
+
+
+
